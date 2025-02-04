8d173c6fe9f5d7da4985d6a2aea71eabfff9b59f
Luis Felipe Febles Castro <luisfelipefeblescastro@gmail.com>
2025-02-04 15:03:58 -0400

    Inicio paquetes

---
+++ ApiMS/ApiMS.Core/Entities/BaseEntity.cs
@@ -0,0 +1,17 @@
+﻿using System.Diagnostics.CodeAnalysis;
+
+namespace ApiMS.Core.Entities;
+
+[ExcludeFromCodeCoverage]
+public class BaseEntity
+{
+    public Guid Id { get; set; }
+
+    public DateTime CreatedAt { get; set; }
+
+    public string? CreatedBy { get; set; }
+
+    public DateTime? UpdatedAt { get; set; }
+
+    public string? UpdatedBy { get; set; }
+}
